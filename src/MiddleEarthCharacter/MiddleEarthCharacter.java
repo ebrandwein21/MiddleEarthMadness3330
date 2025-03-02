ffc3c9994d5d48d443fca8217f6076043e5415c9
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-03-02 17:55:24 -0600

    added fields, constructors, getters/setters to MiddleEarthCharacter

---
+++ src/MiddleEarthCharacter/MiddleEarthCharacter.java
@@ -0,0 +1,43 @@
+package MiddleEarthCharacter;
+
+public abstract class MiddleEarthCharacter {
+
+	private String name;
+	private double health;
+	private double power;
+	
+	/*
+	 * this is a parameterized constructor that sets name, health and power to its current instance
+	 */
+	public MiddleEarthCharacter(String name, double health, double power) {
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setBook(String name) {
+		this.name = name;
+	}
+	
+	public double getHealth() {
+		return health;
+	}
+	
+	public void setAuthor(double health) {
+		this.health = health;
+	}
+	
+	public double getPower() {
+		return power;
+	}
+	
+	public void setPower(double power) {
+		this.power= power;
+	}
+	
+	
+}
