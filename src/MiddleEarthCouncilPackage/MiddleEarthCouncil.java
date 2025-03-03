232fb59ebe6eae44a00c0471e0259eeddf028e52
andergato <145510193+andergato@users.noreply.github.com>
2025-03-03 09:03:02 -0600

    Imported newly created CharacterManager class

--- src/MiddleEarthCouncilPackage/MiddleEarthCouncil.java
+++ src/MiddleEarthCouncilPackage/MiddleEarthCouncil.java
@@ -1,5 +1,7 @@
 package MiddleEarthCouncilPackage;
 
+import middleEarthCharacterManagerPackage.CharacterManager;
+
 public class MiddleEarthCouncil {
 	 
 	private static MiddleEarthCouncil instance;
