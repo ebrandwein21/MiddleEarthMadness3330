a465b3e6d4d17b3ef64a0d8fce155e267654e733
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-03-02 19:14:26 -0600

    started the singleton class and implemented the getinstance method to return the single instance

--- src/MiddleEarthCouncilPackage/MiddleEarthCouncil.java
+++ src/MiddleEarthCouncilPackage/MiddleEarthCouncil.java
@@ -1,5 +1,23 @@
 package MiddleEarthCouncilPackage;
 
 public class MiddleEarthCouncil {
-
+	 
+	private static MiddleEarthCouncil instance;
+	
+	private MiddleEarthCouncil()
+	{
+		
+	}
+	
+	private static MiddleEarthCouncil getInstance()
+	{
+		if(instance == null) {
+			instance = new MiddleEarthCouncil();
+			
+		}
+		return instance;
+	}
+	
+	
+	
 }
