b955f444a6656b3678df91adca588a145df4fd15
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-03-02 18:51:40 -0600

    added target attribute to subclasses

--- src/MiddleEarthCharacter/Orc.java
+++ src/MiddleEarthCharacter/Orc.java
@@ -5,8 +5,10 @@ public abstract class Orc extends MiddleEarthCharacter {
 		String name;
 		double health;
 		double power;
+		MiddleEarthCharacter target;
 		
-		public Orc(String name, double health, double power) {
+		public Orc(String name, double health, double power, MiddleEarthCharacter target) {
 			super(name, health, power);
+			this.target = target;
 		}
 }
