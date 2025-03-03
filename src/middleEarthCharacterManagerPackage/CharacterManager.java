85842bdb23d32351b0d81865210d93a12867b714
andergato <145510193+andergato@users.noreply.github.com>
2025-03-03 09:01:16 -0600

    Wrote skeleton code for the CharacterManager class. Imported MiddleEarthCharacter class, created constructor, and added a skeleton of each method to be implemented.

--- src/middleEarthCharacterManagerPackage/CharacterManager.java
+++ src/middleEarthCharacterManagerPackage/CharacterManager.java
@@ -1,5 +1,37 @@
 package middleEarthCharacterManagerPackage;
 
-public class CharacterManager {
+import MiddleEarthCharacter.MiddleEarthCharacter;
 
+public class CharacterManager {
+	
+	MiddleEarthCharacter[] characters;
+	int size;
+	
+	private CharacterManager(MiddleEarthCharacter[] characters, int size) {
+		this.characters = characters;
+		this.size = size;
+	}
+	
+	boolean addCharacter(MiddleEarthCharacter c){
+		return true;
+	}
+	
+	MiddleEarthCharacter getCharacter(String name){
+		return null;
+	}
+	
+	boolean updateCharacter(MiddleEarthCharacter character, String name, int health, int power){
+		return true;
+	}
+	
+	boolean deleteCharacter(MiddleEarthCharacter character) {
+		return true;
+	}
+	
+	void displayAllCharacters() {
+		
+	}
+	
+	
+	
 }
