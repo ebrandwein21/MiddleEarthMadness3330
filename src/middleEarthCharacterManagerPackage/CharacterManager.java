bc8b284e2a923b86ff37f7d0b02f81243738221c
andergato <145510193+andergato@users.noreply.github.com>
2025-03-04 17:04:18 -0600

    Implemented addCharacter Method
    Fully implemented addCharacter method so that it checks if both the
    array is null and if the array is full. It will return true on success
    and false on failure. Also added JavaDoc for this function.

--- src/middleEarthCharacterManagerPackage/CharacterManager.java
+++ src/middleEarthCharacterManagerPackage/CharacterManager.java
@@ -1,22 +1,52 @@
 package middleEarthCharacterManagerPackage;
 
 import MiddleEarthCharacterPackage.MiddleEarthCharacter;
+import MiddleEarthCharacterPackage.Orc;
 
 public class CharacterManager {
 	
 	MiddleEarthCharacter[] characters;
 	int size;
 	
-	private CharacterManager(MiddleEarthCharacter[] characters, int size) {
-		this.characters = characters;
-		this.size = size;
+	public CharacterManager() {
+		
 	}
 	
-	boolean addCharacter(MiddleEarthCharacter c){
+	//Below constructor is commented out because not sure yet if it will be needed in final version.
+	
+	
+//	/**
+//	 * 
+//	 * this is a parameterized constructor that sets characters and size to its current instance
+//	 * @param characters, size
+//	 * Characters represents the character array and size represents the size of the array.
+//	 */
+//	public CharacterManager(MiddleEarthCharacter[] characters, int size) {
+//		this.characters = characters;
+//		this.size = size;
+//	}
+	
+	
+	
+	/**
+	 * This is a method allowing the user to add a character to the character array. If the array is null,
+	 * it will instantiate a new array and if it is full it will create a new one double the size and copy
+	 * the information over.
+	 * @param c
+	 * Character the user would like to add to the array.
+	 * @return True on success and false on failure.
+	 */
+	public boolean addCharacter(MiddleEarthCharacter c){
+		if(characters == null) {
+			MiddleEarthCharacter[] newArray = new MiddleEarthCharacter[1];
+			characters = newArray;
+		}
 		if(characters.length == size){
 			MiddleEarthCharacter[] newArray = new MiddleEarthCharacter[2 * characters.length];
-			System.arraycopy(c, 0, newArray, 0, size);
+			System.arraycopy(characters, 0, newArray, 0, size);
+			characters = newArray;
 		}
+		
 		characters[size] = c;
 		if(characters[size] == c) {
 			size++;
