2c1411e9b063f740716fde5f93e2f2567153b3d0
andergato <145510193+andergato@users.noreply.github.com>
2025-03-05 15:26:51 -0600

    Created skeleton of MiddleEarthApp Interface
    Created skeleton of MiddleEarthApp Interface, including all the options
    a user can choose from, and all of the if statements that go along with
    that.

--- src/MiddleEarthppPackage/MiddleEarthApp.java
+++ src/MiddleEarthppPackage/MiddleEarthApp.java
@@ -1,14 +1,47 @@
 package MiddleEarthppPackage;
 
-import MiddleEarthCharacterPackage.Dwarf;
-import MiddleEarthCharacterPackage.Elf;
-import MiddleEarthCharacterPackage.Human;
-import MiddleEarthCharacterPackage.Orc;
-import MiddleEarthCharacterPackage.Wizard;
+import MiddleEarthCouncilPackage.MiddleEarthCouncil;
+import middleEarthCharacterManagerPackage.CharacterManager;
+
+import java.util.Scanner;
 
 public class MiddleEarthApp {
-		//this is a test to see if the character class works
 		
-	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		MiddleEarthCouncil council = MiddleEarthCouncil.getInstance();
+		CharacterManager cm = council.getCharacterManager();
+		System.out.println("Hello, welcome to Middle Earth!");
+		while(true){
+			System.out.println("Would you like to: ");
+			System.out.println("1. Add a new character");
+			System.out.println("2. View all characters");
+			System.out.println("3. Update a character");
+			System.out.println("4. Delete a character");
+			System.out.println("5. Exceucte all characters' attack actions");
+			System.out.println("6. Exit");
+			
+			if(scanner.nextInt() == 1) {
+				
+			}
+			if(scanner.nextInt() == 2) {
+				
+			}
+			if(scanner.nextInt() == 3) {
+				
+			}
+			if(scanner.nextInt() == 4) {
+				
+			}
+			if(scanner.nextInt() == 5) {
+				
+			}
+			if(scanner.nextInt() == 6) {
+				break;
+			}
+		}
+		scanner.close();
+	}
+		
 }
 
